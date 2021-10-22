2ab4ffdc564a348d6a00852dec4bf09d53fdefd8
gstachni <gstachni2@yahoo.com>
2021-10-22 11:09:10 -0700

    Updated streaming app client to connect to a Nebula cluster

--- src/main/java/org/gridgain/demo/StreamingApplication.java
+++ src/main/java/org/gridgain/demo/StreamingApplication.java
@@ -16,6 +16,8 @@
  */
 package org.gridgain.demo;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.apache.ignite.Ignite;
@@ -24,11 +26,18 @@ import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.query.SqlFieldsQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.plugin.security.SecurityCredentials;
+import org.apache.ignite.plugin.security.SecurityCredentialsBasicProvider;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.apache.ignite.spi.tracing.opencensus.OpenCensusTracingSpi;
+import org.apache.ignite.ssl.SslContextFactory;
+import org.gridgain.grid.configuration.GridGainConfiguration;
 
 public class StreamingApplication {
     /* Application default execution time. */
-    private static int DEFAULT_EXEC_TIME_MINS = 15;
+    private static final int DEFAULT_EXEC_TIME_MINS = 60;
 
     /* Ignite client instance. */
     private static Ignite ignite;
@@ -37,27 +46,52 @@ public class StreamingApplication {
     private static MarketTicker ticker;
 
     public static void main(String args[]) {
-        int execTime = DEFAULT_EXEC_TIME_MINS;
+        long execTime = DEFAULT_EXEC_TIME_MINS;
+        String address = "e76064f8-2997-42fc-9fa8-a0d0f62397a4.gridgain-nebula-test.com:47500";
+        String login = "test";
+        String password = "testing1";
 
         if (args != null) {
             for (String arg : args) {
                 if (arg.startsWith("execTime")) {
                     execTime = Integer.parseInt(arg.split("=")[1]);
+                } else if (arg.startsWith("address")) {
+                    address = arg.split("=")[1];
                 } else {
-                    System.err.println("Unsupported parameter: " + execTime);
+                    System.err.println("Unsupported parameter: " + arg);
                     return;
                 }
             }
         }
 
+        if (address == null) {
+            System.err.println("ERROR: please provide an address: address=<your cluster address>");
+        }
+
         System.out.println("Application execution time: " + execTime + " minutes");
+        System.out.println("Cluster address to connect: " + address + " minutes");
 
-        // Starting Ignite.
-        IgniteConfiguration cfg = new IgniteConfiguration();
+        System.setProperty("IGNITE_EVENT_DRIVEN_SERVICE_PROCESSOR_ENABLED", "true");
+        System.setProperty("IGNITE_SKIP_CONFIGURATION_CONSISTENCY_CHECK", "true");
 
-        cfg.setClientMode(true);
-        cfg.setPeerClassLoadingEnabled(true);
-        cfg.setTracingSpi(new OpenCensusTracingSpi());
+        IgniteConfiguration cfg = new IgniteConfiguration()
+                .setClientMode(true)
+                .setDiscoverySpi(new TcpDiscoverySpi()
+                    .setIpFinder(new TcpDiscoveryVmIpFinder()
+ //                       .setAddresses(Collections.singleton("70592604-8554-4b18-82b6-8606c4d41551.gridgain-nebula-test.com:47500"))))
+                        .setAddresses(Collections.singleton(address))))
+                .setCommunicationSpi(new TcpCommunicationSpi()
+                    .setForceClientToServerConnections(true))
+                .setPluginConfigurations(new GridGainConfiguration()
+                        .setSecurityCredentialsProvider(new SecurityCredentialsBasicProvider(
+                                new SecurityCredentials(login , password)))
+                .setRollingUpdatesEnabled(true)
+        );
+
+
+// Then use the following snippet to connect the client.
+// Replace the {login} and {password} with your cluster credentials.
+
 
         ignite = Ignition.start(cfg);
 
